cda234fd70e7bea743c3049ac9c0c390bd432832
Birk Weiberg <birk@keyframe.org>
2021-04-17 13:05:12 +0200

    entity select

--- data.js
+++ data.js
@@ -1,19 +1,42 @@
+var entities;
+var segments;
+
 let request = new XMLHttpRequest();
 request.open('GET', 'pellaton.json');
 request.responseType = 'json';
 request.send();
 request.onload = function() {
     var content = request.response;
-    show_entities(content['entities'])
+    entities = content['entities'];
+    segments = content['segments'];
+    show_entities();
 }
 
-function show_entities(entities) {
+function show_entities() {
     var entities_div = document.getElementById('entities');
     var entities_list = document.createElement('ul');
     entities_div.appendChild(entities_list);
     for (var i = 0; i < entities.length; i++) { 
         var entity = document.createElement('li');
+        entity._data = entities[i];
         entity.innerHTML = entities[i].name;
+        entity.addEventListener('click', function () {
+            on_entity_select(this);
+        }, true);
         entities_list.appendChild(entity);
+    }
+}
+
+function on_entity_select(entity) {
+    console.log("click " + entity._data.name);
+    for (var i = 0; i < segments.length; i++) { 
+        if (typeof segments[i].entities == "object") {
+            for (var e in segments[i].entities) {
+                if (segments[i].entities[e] == entity._data.name) {
+                    console.log(segments[i].start);
+                    break;
+                }
+            }
+        }
     }
 }
